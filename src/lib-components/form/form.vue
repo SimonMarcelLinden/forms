d01540b3b324a64215550dec900df487f15c33c7
SimonMarcelLinden <info@snerve.de>
2021-07-28 13:59:48 +0200

    Basic form

---
+++ src/lib-components/form/form.vue
@@ -0,0 +1,8 @@
+<template>
+  <form class="form form-custom" @reset="onReset" @submit="onSubmit">
+    <slot />
+  </form>
+</template>
+
+<script src="./form.ts" lang="ts"></script>
+<style src="./form.css" />
