95921672f2e26e96f083d4739853531420c7dab9
SimonMarcelLinden <info@snerve.de>
2021-07-30 14:34:35 +0200

    Removed files

--- src/lib-components/form-select-option/form-select-option.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <option v-if="isObject(option)" :value="option['value']">{{ option.text }}</option>
-  <option v-else :value="option">{{ option }}</option>
-</template>
-
-<script src="./form-select-option.ts" lang="ts"></script>
